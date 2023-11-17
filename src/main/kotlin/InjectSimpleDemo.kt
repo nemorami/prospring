269ea7ebc49dd99b49f7788dd7d925240f30fde8
nemorami <nemorami@daum.net>
2023-11-17 18:01:14 +0900

    Listing 3-19 field injection

--- src/main/kotlin/InjectSimpleDemo.kt
+++ src/main/kotlin/InjectSimpleDemo.kt
@@ -1,4 +1,5 @@
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.stereotype.Component
 
 @Component("injectSimple")
@@ -8,5 +9,34 @@ class InjectSimpleDemo {
 
     @Value("40")
     private val age = 0
+
+    @Value("1.92")
+    private val height = 0f
+
+    @Value("false")
+    private val developer = false
+
+    @Value("1241401112")
+    private val AgeInSeconds: Long? = null
+    override fun toString(): String{
+        return """
+            Name: $name
+            Age: $age
+            Age in Seconds: $AgeInSeconds
+            Height: $height
+            Is Developer?: $developer
+            """.trimIndent()
+    }
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val ctx = AnnotationConfigApplicationContext()
+            ctx.register(InjectSimpleDemo::class.java)
+            ctx.refresh()
+            val simple = ctx.getBean("injectSimple") as InjectSimpleDemo
+            println(simple)
+        }
+    }
 
 }
